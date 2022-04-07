b5056eaeb855d623503b75f6d80280ceec66b663
Mariana Gallegos <xmarianagallegos@gmail.com>
2022-04-07 18:18:30 -0500

    chale dude

--- js/sw-acces.js
+++ js/sw-acces.js
@@ -1,12 +1,18 @@
 function actualizaCacheDinamico(dynamicCache, request, response){
     if(response.ok){
         //la respuesta tiene data y se debe almacaenar en cache
-        return caches.open(dynamicCache).then(cache => {
+        caches.open(dynamicCache).then(cache => {
             cache.put(request,response.clone() );
-            return cache.clone();
+           
         });
+        return dynamicCache.clone();
     }else{//si no viene nda en la respuesta
+        
         return response;
+    
+    
     }
+
+
 }
 
